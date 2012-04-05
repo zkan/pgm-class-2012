223589fec7a9f76e7a575f55a8bbee031d33e0a8
Kan O <zkan.cs@gmail.com>
2012-04-05 21:09:10 +0700

    update README

--- PGM_Programming_Assignment_2/phenotypeGivenGenotypeFactor.m
+++ PGM_Programming_Assignment_2/phenotypeGivenGenotypeFactor.m
@@ -35,9 +35,19 @@ phenotypeFactor = struct('var', [], 'card', [], 'val', []);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 
 % Fill in phenotypeFactor.var.  This should be a 1-D row vector.
+phenotypeFactor.var = [phenotypeVar, genotypeVar];
+
 % Fill in phenotypeFactor.card.  This should be a 1-D row vector.
+phenotypeFactor.card = [2, numel(alphaList)];
 
 phenotypeFactor.val = zeros(1, prod(phenotypeFactor.card));
 % Replace the zeros in phentoypeFactor.val with the correct values.
+
+idx = 1;
+for i = 1:numel(alphaList)
+    phenotypeFactor.val(idx) = alphaList(i);
+    phenotypeFactor.val(idx + 1) = 1 - alphaList(i);
+    idx = idx + 2;
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- PGM_Programming_Assignment_2/sampleGeneticNetworks.m
+++ PGM_Programming_Assignment_2/sampleGeneticNetworks.m
@@ -17,8 +17,8 @@ phenotypeFactor = phenotypeGivenGenotypeMendelianFactor(isDominant, genotypeVar,
 alphaList = [0.8; 0.6; 0.1];
 genotypeVar = 1;
 phenotypeVar = 3;
-phenotypeFactorAlpha = struct('var', [3,1], 'card', [2,3], 'val', [0.8,0.2,0.6,0.4,0.1,0.9]); % Comment out this line for testing
-% phenotypeFactorAlpha = phenotypeGivenGenotypeFactor(alphaList, genotypeVar, phenotypeVar);
+%phenotypeFactorAlpha = struct('var', [3,1], 'card', [2,3], 'val', [0.8,0.2,0.6,0.4,0.1,0.9]); % Comment out this line for testing
+phenotypeFactorAlpha = phenotypeGivenGenotypeFactor(alphaList, genotypeVar, phenotypeVar);
 
 % Testing genotypeGivenAlleleFreqsFactor:
 alleleFreqs = [0.1; 0.9];
